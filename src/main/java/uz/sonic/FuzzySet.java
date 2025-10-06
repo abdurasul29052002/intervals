ca6b7eebdea3d7c59c87a15414271d93ee28b4a0
E209 <e209@tuiz.uz>
2025-10-06 14:16:06 +0500

    Boilerplate code of Fuzzy Set has been written

---
+++ src/main/java/uz/sonic/FuzzySet.java
@@ -0,0 +1,14 @@
+package uz.sonic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class FuzzySet {
+    private double value;
+    private double left;
+    private double right;
+
+
+}
